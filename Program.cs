7d184ed2c8f815708df7b3dd805c5aed715fd1ff
Дмитрий Сафронов <labs.pecggtu.152.0@outlook.com>
2022-12-03 13:40:23 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Profess.Day5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // while - цикл с предусловием
+
+            //int a = 1;
+            //Console.WriteLine($"Входное значение {a}");
+            //while (a < 10)
+            //{
+            //    a = a * 2;
+            //    Console.WriteLine(a);
+            //}
+
+            //Console.WriteLine($"Конечное значение: {a}");
+
+            // for - цикл со счетчиком
+            for (int i = 1; i < 10; i = i * 2)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
